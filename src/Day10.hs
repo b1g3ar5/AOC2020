df30491dacd743761341fedcc26d40d33d2239d4
Nick Straw <nick@broxholme.com>
2020-12-10 15:36:14 +0000

    Day10

--- src/Day10.hs
+++ src/Day10.hs
@@ -7,43 +7,45 @@ import Memo ( memoise )
 import System.TimeIt ( timeIt )
 
 
-count1 :: [Int] -> Int
-count1 ns = head ls * ls!!1
+countGaps :: [Int] -> Int
+countGaps ns = head ls * ls!!1
   where
     xs = sort ns
     ls = length <$> group (sort $ zipWith (-) (tail xs) (init xs))
 
+
 -- For the split lists when none of the gaps are 3
-count2 :: [Int] -> Int -> Int
-count2 xs n = go [head ys] (tail ys)
+countPerms :: [Int] -> Int
+countPerms ys = go [head ys] (tail ys)
   where
-    ys = drop n xs 
+    --ys = drop startix xs 
     go :: [Int] -> [Int] -> Int
     go _ [] = 1
     go acc [x] = if x > last acc + 3 then 0 else 1
+    -- We can either use the next one or skip it
     go acc rem = go (acc ++ [head rem]) (tail rem) + go acc (tail rem)
 
 
 -- Naive solution of the whole lot - to test memoisation
-count3 :: [Int] -> Int -> Int
-count3 xs n = fromIntegral $ memoisedGo 0
+countPerms2 :: [Int] -> Int
+countPerms2 ys = fromIntegral $ memoisedGo 0
   where
-    ys = drop n xs 
-
-    memoisedGo = memoise go toInteger fromIntegral toInteger fromIntegral
+    -- Memoise for recursive functions
+    memoisedGo = memoise go id id id id 
 
     -- Recursion factored out...
-    go :: (Int -> Int) -> Int -> Int
-    go f i
-      | i == (l - 1) = 1
-      | otherwise = foldl' (\acc x -> acc + f x) 0 js
+    go :: (Integer -> Integer) -> Integer -> Integer
+    go f i = if i == fromIntegral (l - 1) then 1 else foldl' (\acc x -> acc + f x) 0 js
       where
-        x = xs!!i
-        js = filter (\k -> xs!!k - x <=3) [(i+1)..(l-1)]
+        ii = fromIntegral i
         l = length ys
+        x = ys!!ii
+        -- possible next items...
+        js = toInteger <$> filter (\k -> ys!!k - x <=3) [(ii+1)..(l-1)]
 
 
 
+-- Split at all gaps of 3 - so we can just
 split :: [Int] -> [[Int]]
 split xs = reverse $ go (head xs) [] [head xs] (tail xs)
   where
@@ -60,10 +62,8 @@ day10 = do
   let ns :: [Int]
       ns = read <$> ls
       xs = sort $ 0:ns ++ [maximum ns + 3]
-      splits = split xs
-      cs = flip count2 0 <$> splits
 
   
-  timeIt $ putStrLn $ "Day10: part1: " ++ show (count1 xs)
-  timeIt $ putStrLn $ "Day10: part2: splitting: " ++ show (product cs)
-  timeIt $ putStrLn $ "Day10: part2: memoisation: " ++ show (count3 xs 0)
+  timeIt $ putStrLn $ "Day10: part1: " ++ show (countGaps xs)
+  timeIt $ putStrLn $ "Day10: part2: splitting: " ++ show (product $ countPerms <$> split xs)
+  timeIt $ putStrLn $ "Day10: part2: memoisation: " ++ show (countPerms2 xs)
